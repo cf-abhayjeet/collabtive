839f8a2c4707adb2bd7120a9486bd482d39527d7
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2013-01-08 14:53:18 +0100

    Added heroky db support

--- config/standard/config.php
+++ config/standard/config.php
@@ -1,10 +1,21 @@
 <?php
+
+if (isset($_SERVER["DATABASE_URL"])) {
+ $db = parse_url($_SERVER["DATABASE_URL"]);
+ define("DB_NAME", trim($db["path"],"/"));
+ define("DB_USER", $db["user"]);
+ define("DB_PASSWORD", $db["pass"]);
+ define("DB_HOST", $db["host"]);
+}
+else {
+ die("Your heroku DATABASE_URL does not appear to be correctly specified.");
+}
+
+/* Not used, see above
 $db_host = 'XXXX';
-
 $db_name = 'XXXX';
-
 $db_user = 'XXXX';
-
 $db_pass = 'XXXX';
+*/
 
 ?>
